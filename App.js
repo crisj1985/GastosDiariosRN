f80f5daff2e6fea60de7c28b5b4b7a8c66e8bc42
crisj1985 <crisj1985@hotmail.com>
2020-04-22 12:17:38 -0500

    navegacion tab

--- App.js
+++ App.js
@@ -1,34 +1,95 @@
 import React, { Component } from 'react';
 import {createStackNavigator} from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import { Ingreso } from "./Screens/IngresosScreen";
 import { Gasto } from "./Screens/GastosScreen";
 import { Home } from "./Screens/Home";
+import { Icon } from "react-native-elements";
 
-let NavStack = createStackNavigator();
+// let NavStack = createStackNavigator();
+let NavTab = createBottomTabNavigator();
 
 export default class App extends Component {
 render(){
   return (
     <NavigationContainer>
-      <NavStack.Navigator initialRouteName="StackHome">
-        <NavStack.Screen
-          options={{ title: "Agregar Ingresos" }}
-          name="StackIngresos"
-          component={Ingreso}
-        />
-        <NavStack.Screen
-          options={{ title: "Agregar Gastos" }}
-          name="StackGastos"
-          component={Gasto}
-        />
-        <NavStack.Screen
-          options={{ title: "Gastos Diarios" }}
+      <NavTab.Navigator initialRouteName="StackHome">
+        <NavTab.Screen
+          options={{
+            tabBarLabel: "Gastos Diarios",
+            tabBarIcon: () => {
+              return (
+                <Icon
+                  reverse
+                  name="home"
+                  type="font-awesome"
+                  color="#517fa4"
+                  size={16}
+                />
+              );
+            },
+          }}
           name="StackHome"
           component={Home}
         />
-      </NavStack.Navigator>
+        <NavTab.Screen
+          options={{
+            tabBarLabel: "Agregar Ingresos",
+            tabBarIcon: () => {
+              return (
+                <Icon
+                  reverse
+                  name="plus"
+                  type="font-awesome"
+                  color="#517fa4"
+                  size={16}
+                />
+              );
+            },
+          }}
+          name="StackIngresos"
+          component={Ingreso}
+        />
+        <NavTab.Screen
+          options={{
+            tabBarLabel: "Agregar Gastos",
+            tabBarIcon: () => {
+              return (
+                <Icon
+                  reverse
+                  name="minus"
+                  type="font-awesome"
+                  color="#517fa4"
+                  size={16}
+                />
+              );
+            },
+          }}
+          name="StackGastos"
+          component={Gasto}
+        />
+      </NavTab.Navigator>
     </NavigationContainer>
+    // <NavigationContainer>
+    //   <NavStack.Navigator initialRouteName="StackHome">
+    //     <NavStack.Screen
+    //       options={{ title: "Agregar Ingresos" }}
+    //       name="StackIngresos"
+    //       component={Ingreso}
+    //     />
+    //     <NavStack.Screen
+    //       options={{ title: "Agregar Gastos" }}
+    //       name="StackGastos"
+    //       component={Gasto}
+    //     />
+    //     <NavStack.Screen
+    //       options={{ title: "Gastos Diarios" }}
+    //       name="StackHome"
+    //       component={Home}
+    //     />
+    //   </NavStack.Navigator>
+    // </NavigationContainer>
   );
 }
 }
